9e7c12cfa3e6cfd4a18792953d41c4e7abe23eaa
AJLeonardi <anthony.leonardi@gmail.com>
2013-04-04 09:12:37 -0400

    added controller test skeleton?

---
+++ my_rampup_app/spec/controllers/user_c_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe users_controller do
+	context "User render templates" do
+		it "should render the new template when new called" do
+		end
+		it "should redirect to root when user creation success" do
+		end
+		it "should flash success message when user creation success" do
+		end
+		it "should render new template when user creation fails" do
+		end
+	end
+end
